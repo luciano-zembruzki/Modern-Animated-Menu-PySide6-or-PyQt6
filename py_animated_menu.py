c8a6a842334476731e726545dfecdcd740e2ef6a
Professor Luciano <86445195+professorlucianoz@users.noreply.github.com>
2022-04-16 14:35:02 -0300

    Initial commit

---
+++ py_animated_menu.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'animated_menu_uioDSFkG.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
+    QMainWindow, QSizePolicy, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setStyleSheet(u"QWidget#centralwidget{\n"
+"	background-color: qlineargradient(spread:pad, x1:0, y1:1, x2:1, y2:0, stop:0 rgba(255, 178, 102, 255), stop:0.98 rgba(255, 85, 255, 255), stop:1 rgba(0, 0, 0, 0));\n"
+"}\n"
+"\n"
+"QFrame#menu_frame{\n"
+"	border-right: 2px;\n"
+"	border-style: solid;\n"
+"	border-color: #7E8080;\n"
+"}")
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.menu_frame = QFrame(self.centralwidget)
+        self.menu_frame.setObjectName(u"menu_frame")
+        self.menu_frame.setMaximumSize(QSize(50, 16777215))
+        self.menu_frame.setStyleSheet(u"")
+        self.menu_frame.setFrameShape(QFrame.StyledPanel)
+        self.menu_frame.setFrameShadow(QFrame.Raised)
+
+        self.horizontalLayout.addWidget(self.menu_frame)
+
+        self.content_frame = QFrame(self.centralwidget)
+        self.content_frame.setObjectName(u"content_frame")
+        self.content_frame.setStyleSheet(u"")
+        self.content_frame.setFrameShape(QFrame.StyledPanel)
+        self.content_frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.content_frame)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label = QLabel(self.content_frame)
+        self.label.setObjectName(u"label")
+        font = QFont()
+        font.setPointSize(18)
+        font.setBold(True)
+        font.setItalic(True)
+        self.label.setFont(font)
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.horizontalLayout_2.addWidget(self.label)
+
+
+        self.horizontalLayout.addWidget(self.content_frame)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\"><span style=\" font-size:18pt; font-weight:700; font-style:italic; color:#ffffff;\">Frame de conte\u00fado</span></p></body></html>", None))
+    # retranslateUi
+
